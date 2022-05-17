eec7b2e23525d28e1435a33e6f9e027f5273191c
Renato <renatoburgos49@gmail.com>
2022-05-17 19:29:54 -0400

    Cambios

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,35 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-import com.google.android.gms.maps.OnMapReadyCallback;
+public class MainActivity extends AppCompatActivity implements GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener,
+        OnMapReadyCallback {
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-
+    private static final int TAG_CODE_PERMISSION_LOCATION = 1;
     private GoogleMap mMap;
 
     ImageButton btnGPS;
@@ -46,49 +38,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
-
-        btnGPS = findViewById(R.id.gps);
-        btnGPS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LocationManager locationManager = (LocationManager) MainActivity.this.getSystemService(Context.LOCATION_SERVICE);
-                LocationListener locationListener = new LocationListener() {
-                    @SuppressLint("MissingPermission")
-                    @Override
-                    public void onLocationChanged(@NonNull Location location) {
-                        LatLng lugar = new LatLng(location.getLatitude(), location.getLongitude());
-                        /*mMap.addMarker(new MarkerOptions()
-                                .position(lugar)
-                                .title("Marker in Sydney"));*/
-                        mMap.moveCamera(CameraUpdateFactory
-                                .newLatLngZoom(lugar, 16.0f));
-                        mMap.setMyLocationEnabled(true);
-                    }
-
-                    /*public void onStatusChanged(String provider, int status, Bundle extras) {
-                    }
-
-                    public void onProviderEnabled(String provider) {
-                    }
-
-                    public void onProviderDisabled(String provider) {
-                    }*/
-                };
-
-                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-            }
-        });
-
-        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
-
-        if(permissionCheck == PackageManager.PERMISSION_DENIED){
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){}
-            else{
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            }
-        }
 
         /*final Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener(){
@@ -98,18 +49,31 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         });*/
         //button.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, SecondActivity.class)));
     }
+    @SuppressLint("MissingPermission")
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap; boolean success;
-        //success = googleMap.setMapStyle(new MapStyleOptions(getResources().getString(R.string.style_json)));
-        //success = googleMap.setMapStyle(null);
+    public void onMapReady(GoogleMap map) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+            boolean success = map.setMapStyle(new MapStyleOptions(getResources().getString(R.string.dia_json)));
+            map.setMyLocationEnabled(true);
+            map.getUiSettings().setMyLocationButtonEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION },
+                    TAG_CODE_PERMISSION_LOCATION);
+        }
+    }
 
-        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
-            //success = googleMap.setMapStyle(new MapStyleOptions(getResources().getString(R.string.style_json)));
-        }
-        else {
-        }
-            //setTheme(R.style.dayTheme);
-            //success = googleMap.setMapStyle(null);
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+
     }
 }
