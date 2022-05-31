80e9b661302ac63592446423717988d867a83126
grimlin31 <nico.bernal.al@gmail.com>
2022-05-31 09:39:21 -0400

    -fix: register view

--- app/src/main/java/com/grimlin31/buddywalkowner/LoginActivity.java
+++ app/src/main/java/com/grimlin31/buddywalkowner/LoginActivity.java
@@ -1,16 +1,11 @@
 package com.grimlin31.buddywalkowner;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.grimlin31.buddywalkowner.register.RegisterActivity;
-import com.grimlin31.buddywalkowner.register.UserForm;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BuddyWalkerAppCompactActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +16,9 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-
-        View decorView = getWindow().getDecorView();
-        // Hide the status bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
-        // Remember that you should never show the action bar if the
-        // status bar is hidden, so hide that too if necessary.
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
     }
 
     public void toRegister(View view) {
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
+        super.nextActivity(RegisterActivity.class);
     }
 }
--- app/src/main/java/com/grimlin31/buddywalkowner/register/RegisterActivity.java
+++ app/src/main/java/com/grimlin31/buddywalkowner/register/RegisterActivity.java
@@ -1,41 +1,33 @@
 package com.grimlin31.buddywalkowner.register;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 
+import com.grimlin31.buddywalkowner.BuddyWalkerAppCompactActivity;
+import com.grimlin31.buddywalkowner.LoginActivity;
 import com.grimlin31.buddywalkowner.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends BuddyWalkerAppCompactActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        View decorView = getWindow().getDecorView();
-        // Hide the status bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
-        // Remember that you should never show the action bar if the
-        // status bar is hidden, so hide that too if necessary.
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
     }
 
-    public void nextFormDataUser() {
-        Intent nextForm = new Intent(this, UserForm.class);
-        startActivity(nextForm);
+    public void nextFormDataUser(View view)  {
+        super.nextActivity(UserForm.class);
+    }
+
+    public void toLogin(View view)  {
+        super.nextActivity(LoginActivity.class);
     }
 
 }
